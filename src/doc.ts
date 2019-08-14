eeaab159b36a1ac879ecbbccde26bbc0c5be14a6
BirdIT <nico-vogel97@outlook.de>
2019-08-15 00:30:53 +0200

    implement first method

---
+++ src/doc.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import { spawn } from 'child_process';
+import * as program from 'commander';
+
+program.version('0.0.1');
+program
+	.command('images')
+	.alias('is')
+	.description('equivalent to "docker images"')
+	.action(() => {
+		const dockerImages = spawn('docker', ['images']);
+		let output = '';
+		dockerImages.stdout.on('data', data => {
+			output = `${output}${data}`;
+		});
+		dockerImages.on('close', () => {
+			console.log(output);
+		});
+	});
+
+// allow commander to parse `process.argv`
+program.parse(process.argv);
