fd11e4ba9912a55135e53f9278618ec4d0d591f0
BirdIT <nico-vogel97@outlook.de>
2019-08-15 23:40:02 +0200

    register network commands

--- src/docker-extension.ts
+++ src/docker-extension.ts
@@ -4,13 +4,15 @@ import * as program from 'commander';
 import { Docker, Options } from 'docker-cli-js';
 import { setupImageCommands } from './images';
 import { setupContainerCommands } from './container';
+import { setupNetworkCommands } from './network';
 
 const options = new Options(undefined, __dirname);
 const docker = new Docker(options);
 
-program.version('0.0.1');
+program.version('0.0.1').description('shorten and extend the docker commands');
 setupImageCommands(program, docker);
 setupContainerCommands(program, docker);
+setupNetworkCommands(program, docker);
 
 // allow commander to parse `process.argv`
 program.parse(process.argv);
