c8a1098ad3085566286436114ef71ab695149f2b
Nico Vogel <nico-vogel97@outlook.de>
2019-08-16 13:54:58 +0200

    fix update script

--- updateVersion.js
+++ updateVersion.js
@@ -6,10 +6,10 @@ const packageFile = fs.readFileSync('package.json').toString();
 const packageData = JSON.parse(packageFile);
 const version = packageData.version;
 
-const dockerExtensionPath = 'src/docker-extension.ts'
+const dockerExtensionPath = 'src/docker-extension.ts';
 const dockerExtensionFile = fs.readFileSync(dockerExtensionPath).toString();
 const dockerExtensionNewFile = dockerExtensionFile.replace(/program\.version\('(.+?)'\)/, `program.version('${version}')`);
 fs.writeFileSync(dockerExtensionPath, dockerExtensionNewFile);
 
-cp.execSync(`git commit -m '${version}'`)
-cp.execSync(`git push`)
+cp.execSync(`git add ${dockerExtensionPath}`);
+cp.execSync(`git commit -m '${version}'`);
