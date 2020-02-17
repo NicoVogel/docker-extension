060d0ff7e64d9f5fa9a724279e6e69b7725316bc
NicoVogel <nico-vogel97@outlook.de>
2020-02-17 11:51:18 +0100

    remove no longer needed files

--- src/container.ts
+++
@@ -1,7 +0,0 @@
-import { HelperCaller } from './helper';
-
-const mappings: Map<string, string> = new Map();
-mappings.set('p', 'prune');
-mappings.set('e', 'exec');
-
-export const container = new HelperCaller('container', 'c', mappings, 'ps');
--- src/images.ts
+++
@@ -1,7 +0,0 @@
-import { HelperCaller } from './helper';
-
-const mappings: Map<string, string> = new Map();
-mappings.set('h', 'history');
-mappings.set('i', 'inspect');
-
-export const image = new HelperCaller('image', 'i', mappings, 'ls');
