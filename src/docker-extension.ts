1a3a74a9b0ac5c03bec6b823bad235042a3cd53e
NicoVogel <nico-vogel97@outlook.de>
2020-02-17 10:17:23 +0100

    forward command if known keword is used

--- src/docker-extension.ts
+++ src/docker-extension.ts
@@ -2,19 +2,67 @@
 
 import { Caller } from './@types/model';
 import { container } from './container';
-import { removeFirstArrayItem } from './helper';
+import { removeFirstItem, runner, removeFirstItems } from './helper';
+import { existsSync } from 'fs';
 
-const callers: Caller[] = [container];
+const removeURLParameter = (args: string[]) => {
+	const clone = [...args];
+	for (let index = 0; index < clone.length; index++) {
+		const arg = clone[index];
+		try {
+			if (existsSync(arg) === false) {
+				break;
+			}
+			clone.shift();
+			index--;
+		} catch (error) {
+			break;
+		}
+	}
+	return clone;
+};
 
-const args = removeFirstArrayItem(process.argv);
+const callerImports = [container];
+const forwardKeywords = [
+	'builder',
+	'config',
+	'container',
+	'context',
+	'image',
+	'network',
+	'node',
+	'plugin',
+	'secret',
+	'service',
+	'stack',
+	'swarm',
+	'system',
+	'trust',
+	'volum'
+]
+const callers: Map<string, Caller> = new Map();
+callerImports.forEach(caller => callers.set(caller.abbriviation(), caller));
+const args = removeURLParameter(process.argv);
+
+console.log(process.argv);
+console.log(args);
+
 
 if (args.length === 0) {
-	// no parameter at all
-	callers[0].invoke([]);
+	console.log('no param');
+	callerImports[0].invoke([]);
 } else {
-	callers.forEach(caller => {
-		if (caller.abbriviation() === args[0]) {
-			caller.invoke(removeFirstArrayItem(args));
-		}
-	});
+	const command = args[0];
+	if (forwardKeywords.some(keyword => keyword === command)) {
+		const action = args[1];
+		const params = removeFirstItems(args, 2);
+		console.log({ command, action, params });
+		runner(command, action, params);
+	}
+	let selectCaller = callers.get(command);
+	if (selectCaller === undefined) {
+		selectCaller = callerImports[0];
+	}
+	console.log(JSON.stringify(selectCaller));
+	selectCaller.invoke(removeFirstItem(args));
 }
