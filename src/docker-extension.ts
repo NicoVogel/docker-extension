ce3a4bc1853f23f7e99cd42974dd28a0286703f2
NicoVogel <nico-vogel97@outlook.de>
2020-02-17 11:53:24 +0100

    use the config to setup the callers

--- src/docker-extension.ts
+++ src/docker-extension.ts
@@ -1,29 +1,36 @@
 #!/usr/bin/env node
 
 import { Caller } from './@types/model';
-import { removeFirstItem, runner, removeFirstItems } from './helper';
-import { existsSync } from 'fs';
-import { container } from './container';
-import { image } from './images';
+import {
+	removeFirstItem,
+	runner,
+	removeFirstItems,
+	HelperCaller
+} from './helper';
+import { readFileSync } from 'fs';
+import { Config } from './@types/config';
+import { dirname, join } from 'path';
 
-const removeURLParameter = (args: string[]) => {
-	const clone = [...args];
-	for (let index = 0; index < clone.length; index++) {
-		const arg = clone[index];
-		try {
-			if (existsSync(arg) === false) {
-				break;
-			}
-			clone.shift();
-			index--;
-		} catch (error) {
-			break;
-		}
+const installLocation = dirname(process.argv[1]);
+const configLocation = join(installLocation, 'config.json');
+const config: Config = JSON.parse(readFileSync(configLocation).toString());
+
+const callers: Map<string, Caller> = new Map();
+for (const key in config.abbrev) {
+	if (config.abbrev.hasOwnProperty(key)) {
+		const data = config.abbrev[key];
+		const caller = new HelperCaller(
+			data.command,
+			key,
+			new Map(data.mappings),
+			data.default,
+			config.showCommand
+		);
+		callers.set(key, caller);
 	}
-	return clone;
-};
+}
+let defaultCaller: Caller = callers.values().next().value;
 
-const callerImports = [container, image];
 const forwardKeywords = [
 	'builder',
 	'config',
@@ -40,23 +47,22 @@ const forwardKeywords = [
 	'system',
 	'trust',
 	'volum'
-]
-const callers: Map<string, Caller> = new Map();
-callerImports.forEach(caller => callers.set(caller.abbriviation(), caller));
-const args = removeURLParameter(process.argv);
+];
+
+const args = removeFirstItems(process.argv, 2);
 if (args.length === 0) {
-	callerImports[0].invoke([]);
+	defaultCaller.invoke([]);
 } else {
 	const command = args[0];
 	if (forwardKeywords.some(keyword => keyword === command)) {
 		const action = args[1];
 		const params = removeFirstItems(args, 2);
 		console.log({ command, action, params });
-		runner(command, action, params);
+		runner(command, action, params, config.showCommand);
 	}
 	let selectCaller = callers.get(command);
 	if (selectCaller === undefined) {
-		selectCaller = callerImports[0];
+		selectCaller = defaultCaller;
 	}
 	selectCaller.invoke(removeFirstItem(args));
 }
