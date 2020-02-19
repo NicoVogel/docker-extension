4d231c4e1a32e017695329fcc789cfb21f78acb6
BirdIT <nico-vogel97@outlook.de>
2020-02-19 21:24:19 +0000

    add comments

--- src/@types/config.d.ts
+++ src/@types/config.d.ts
@@ -1,10 +1,16 @@
 export interface Config {
+	// show the resulting command before executing it
 	showCommand?: boolean;
+	// which command mapping is used if no command is provided
 	default: string;
+	// the abbreviation is the property name
 	commandMappings: {
 		[commandMappings: string]: {
+			// the full command name
 			command: string;
+			// which action is executed by default
 			default: string;
+			// the fist value is the abbreviation and the second the full action
 			actionMappings: [string, string][];
 		};
 	};
