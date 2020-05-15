d0fa0c806e115fa4fc6e41fae0c5d48abc44698d
BirdIT <nico-vogel97@outlook.de>
2020-05-15 18:30:24 +0000

    extend configHandler with configWriter function

--- src/configHandler.ts
+++ src/configHandler.ts
@@ -5,29 +5,51 @@ import { Config } from './@types/config';
 
 
 
-const configLoader = (processUrl: string): Config => {
+export const configLocation = join(dirname(process.argv[1]),
+	'./.config/docker-extension.json')
+
+const createConfigIfNotExists = (data: Config, path: string) => {
 	try {
-		const installLocation = dirname(processUrl);
-		const configLocation = join(
-			installLocation,
-			'./.config/docker-extension.json'
+		mkdirSync(dirname(path), { recursive: true });
+		writeFileSync(path, JSON.stringify(data, null, 2));
+	} catch (err) {
+		throw new Error(
+			`Unable to write default config to location ${path}. Error message: ${err.message}`
 		);
+	}
+}
+
+export const configWriter = (file: string, currentDirectory: string): void => {
+	const originLocation = file.startsWith('/') ? file : join(currentDirectory, file);
+	console.log(originLocation)
+	try {
+		if (existsSync(originLocation) === false) {
+			throw new Error(`the passed origin file path does not exist!, passed path: ${originLocation}`);
+		}
+		let readFileData;
+		try {
+			readFileData = JSON.parse(readFileSync(originLocation).toString());
+		} catch (err) {
+			throw new Error(`Problem while reading the config origin! ${err.message}`);
+		}
+		createConfigIfNotExists(readFileData, configLocation);
+
+	} catch (err) {
+		console.error(`Problem while managing the config file! Fiel path: ${file}\nError: ${err.message}`);
+	}
+}
+
+const configLoader = (): Config => {
+	try {
 		if (existsSync(configLocation) === false) {
-			try {
-				mkdirSync(dirname(configLocation), { recursive: true });
-				writeFileSync(configLocation, JSON.stringify(defaultConfig, null, 2));
-			} catch (err) {
-				throw new Error(
-					`Unable to write default config to location ${configLocation}. Error message: ${err.message}`
-				);
-			}
+			createConfigIfNotExists(defaultConfig, configLocation);
 			return defaultConfig;
 		}
 		try {
 			return JSON.parse(readFileSync(configLocation).toString()) as Config;
 		} catch (err) {
 			throw new Error(
-				`Unable to load and parse the the config file from location ${configLocation}. Error message: ${err.message}`
+				`Unable to load and parse the config file from location ${configLocation}. Error message: ${err.message}`
 			);
 		}
 	} catch (err) {
@@ -40,7 +62,7 @@ let config: Config;
 
 export const getConfig = (): Config => {
 	if (!config) {
-		config = configLoader(process.argv[1]);
+		config = configLoader();
 	}
 	return config;
 }
