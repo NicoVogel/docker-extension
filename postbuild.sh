1cffaffe90a54c583163be63dcf872f9ee22df3b
Nico Vogel <nico-vogel97@outlook.de>
2020-05-18 22:02:39 +0200

    add version number and refactor extension commands

--- postbuild.sh
+++ postbuild.sh
@@ -5,3 +5,18 @@ sed -i -e 's/\#\!\/usr\/bin\/env node//g' $1
 
 # add the node bash execution instrution at the file start
 echo -e "#!/usr/bin/env node\n$(cat $1)" > $1
+
+# source: https://gist.github.com/DarrenN/8c6a5b969481725a4413
+if [[ -z "${PACKAGE_VERSION}" ]]; then
+  PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+else
+  PACKAGE_VERSION=${PACKAGE_VERSION}
+fi
+
+sed -i -e "s/\$PACKAGE_VERSION/$PACKAGE_VERSION/g" $1
+
+
--- src/extensionCommands.ts
+++ src/extensionCommands.ts
@@ -1,29 +1,48 @@
 import { configLocation, configWriter } from './configHandler';
 
-export const evalExtensionCommand = (args: string[], processArgs: string[]) => {
-	if (!args || args.length === 0 || args[0] === 'help') {
-		console.log(
-			`Docker Extension specific commands:
-    help:           show which commands are availiabe
-    get-config:     get the config path
-    save-config:    override the config with a file
+const helpLog = () => {
+	console.log(
+		`Docker Extension specific commands:
+help:           show which commands are availiabe
+get-config:     get the config path
+save-config:    override the config with a file
+version:		get the version number of the current build
 `
+	);
+}
+
+const saveLog = (args: string[]) => {
+	if (args.length === 1) {
+		console.log(
+			'You need to specify a file which will be used as new config'
 		);
 		return;
 	}
+	configWriter(args[1], process.cwd());
+}
 
-	if (args[0] === 'get-config') {
-		console.log(configLocation);
+export const evalExtensionCommand = (args: string[], processArgs: string[]) => {
+	if (!args || args.length === 0) {
+		helpLog();
 		return;
 	}
 
-	if (args[0] === 'save-config') {
-		if (args.length === 1) {
-			console.log(
-				'You need to specify a file which will be used as new config'
-			);
-		}
-		configWriter(args[1], process.cwd());
-		return;
+	switch (args[0]) {
+		case 'get-config':
+			console.log(configLocation);
+			break;
+		case 'version':
+			console.log('docker-extension version:$PACKAGE_VERSION');
+			break;
+		case 'save-config':
+			saveLog(args);
+			break;
+		case 'help':
+			helpLog();
+			break;
+		default:
+			console.error(`Unknown command: ${args[0]}`)
+			helpLog();
+			break;
 	}
 };
