1cb3fbd3e1000de37da3f85f64ba9d4c64c43c92
NicoVogel <nico-vogel97@outlook.de>
2020-05-19 13:30:22 +0000

    fix typo

--- src/@types/config.d.ts
+++ src/@types/config.d.ts
@@ -20,6 +20,6 @@ export interface Config {
 		// The value is a docker command with placeholders
 		[commandMapping: string]: string;
 	};
-	// open in editor command, needs to contain one placeholder $0 for the config gile path
+	// open in editor command, needs to contain one placeholder $0 for the config file path
 	openEditorCommand: string;
 }
